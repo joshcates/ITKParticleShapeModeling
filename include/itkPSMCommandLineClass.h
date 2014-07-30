88ceb0e8736bbc1b2f62a78a7ad485bb28ab28ba
sbengali <salma.bengali@carma.utah.edu>
2014-07-30 13:33:05 -0600

    Added PSM Command Line Class

---
+++ include/itkPSMCommandLineClass.h
@@ -0,0 +1,119 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef ____itkPSMCommandLineClass__
+#define ____itkPSMCommandLineClass__
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "itkImage.h"
+#include "itkPSMProcrustesRegistration.h"
+#include "itkImageFileReader.h"
+#include "itkPSMEntropyModelFilter.h"
+#include "itkPSMProject.h"
+#include "itkPSMProjectReader.h"
+#include "itkPSMParticleSystem.h"
+#include "itkCommand.h"
+
+namespace itk
+{
+/** \class PSMCommandLineClass
+ *  \brief This class provides a command line tool to run the Particle Shape
+ *   Modeling. It runs the optimization as well as Procrustes
+ *   Registration if the option is in the project parameter file. For now, 
+ *   the tool only uses the PSMEntropyModelFilter for the optimization.
+ *
+ *
+ *  \ingroup PSM
+ */
+
+template <unsigned int VDimension>
+class ITK_EXPORT PSMCommandLineClass : public DataObject
+{
+  public:
+    /** Standard class typedefs. */
+    typedef PSMCommandLineClass  Self;
+    typedef DataObject Superclass;
+    typedef SmartPointer<Self>   Pointer;
+    typedef SmartPointer<const Self>  ConstPointer;
+  
+    /** Method for creation through the object factory. */
+    itkNewMacro(Self);
+  
+    /** Run-time type information (and related methods). */
+    itkTypeMacro(PSMCommandLineClass, DataObject);
+  
+    /** Input distance transforms image typedef */
+    typedef typename itk::Image<float, 3> ImageType;
+    /** PSM model optimization filter typedef */
+    typedef PSMEntropyModelFilter<typename PSMCommandLineClass::ImageType> EntropyModelFilterType;
+    /** Procrustes Registration typedef */
+    typedef PSMProcrustesRegistration<3> ProcrustesRegistrationType;
+    /** Project Reader typedef */
+    typedef PSMProjectReader ProjectReaderType;
+    /** Project typedef */
+    typedef PSMProject ProjectType;
+  
+    /** Callback to run Procrustes Registration on the shapes at the interval 
+     *  specified in the project parameter file or by default. */
+    void IterateCallback(itk::Object *, const itk::EventObject &);
+    /** Read the distance transforms that are provided as inputs to the 
+     *  optimzation filter. */
+    void ReadInputs(std::string input_path_prefix);
+    /** Read the input parameters that set various optimization attribute values */
+    void ReadInputParameters();
+    /** Write out the optimized point sets to user specified files */
+    void WriteOutputs(std::string output_path);
+    /** Run the steps of the optimization process */
+    void Run(const char *fname, std::string input_path_prefix, std::string output_path);
+    /** Set the file name of the project parameter file */
+    void SetProjectParameterFileName(const char *fname);
+  
+  //protected:
+    PSMCommandLineClass();
+    virtual ~PSMCommandLineClass() {};
+  
+  private:
+    PSMCommandLineClass(const Self&); //purposely not implemented
+    void operator=(const Self&);      //purposely not implemented
+    /** The project parameter file that contains information about input and output 
+     *  file names and the optimization attribute values */
+    const char *m_ProjectParameterFile;
+    /** The PSM model optimization filter */
+    typename EntropyModelFilterType::Pointer m_Filter;
+    /** The Procrustes Registration filter that will register the point sets */
+    typename ProcrustesRegistrationType::Pointer m_ProcrustesRegistration;
+    /** The reader which will read in the project parameters */
+    typename ProjectReaderType::Pointer m_XmlReader;
+    /** The variable which will store the project parameters */
+    typename ProjectType::Pointer m_Project;
+    /** Counter to keep track of when to run Procrustes Registration */
+    int m_ProcrustesCounter;
+    /** This variable calls a pointer to a member function, in this case, the
+     *  IterateCallback function which will run Procrustes at specified intervals
+     *  during the optimization */
+    typename itk::MemberCommand<PSMCommandLineClass>::Pointer m_IterateCmd;
+};
+} // end namespace
+
+/*#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkPSMCommandLineClass.hxx"
+#endif*/
+
+#endif 
---
+++ src/itkPSMCommandLineClass.cxx
@@ -0,0 +1,272 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+//#ifndef _itkPSMCommandLineClass_hxx
+//#define _itkPSMCommandLineClass_hxx
+
+#include "itkPSMCommandLineClass.h"
+#include <fstream>
+#include <vector>
+
+namespace itk
+{
+template class PSMCommandLineClass<3>;
+
+template <unsigned int VDimension>
+PSMCommandLineClass<VDimension>
+::PSMCommandLineClass()
+{
+  this->m_ProcrustesCounter = 0;
+  //this->m_ProjectParameterFile = fname;
+}
+  
+template <unsigned int VDimension>
+void PSMCommandLineClass<VDimension>
+::IterateCallback(itk::Object *caller , const itk::EventObject &)
+{
+  if (this->m_ProcrustesRegistration->GetProcrustesInterval() != 0)
+  {
+    this->m_ProcrustesCounter++;
+    if (this->m_ProcrustesCounter >= (int)this->m_ProcrustesRegistration->GetProcrustesInterval())
+    {
+      // Reset the counter
+      this->m_ProcrustesCounter = 0;
+      this->m_ProcrustesRegistration->RunRegistration();
+      std::cout << "Run Procrustes Registration" << std::endl;
+    }
+  }
+  PSMEntropyModelFilter<PSMCommandLineClass::ImageType> *o
+  = static_cast<PSMEntropyModelFilter<PSMCommandLineClass::ImageType> *>(caller);
+  // Print every 10 iterations
+  if (o->GetNumberOfElapsedIterations() % 10 != 0) return;
+  
+  std::cout << "Iteration # " << o->GetNumberOfElapsedIterations() << std::endl;
+  std::cout << " Eigenmode variances: ";
+  for (unsigned int i = 0; i < o->GetShapePCAVariances().size(); i++)
+  {
+    std::cout << o->GetShapePCAVariances()[i] << " ";
+  }
+  std::cout << std::endl;
+  std::cout << " Regularization = " << o->GetRegularizationConstant() << std::endl;
+}
+
+template <unsigned int VDimension>
+void PSMCommandLineClass<VDimension>
+::ReadInputs(std::string input_path_prefix)
+{
+  //itk::PSMProjectReader::Pointer m_XmlReader = itk::PSMProjectReader::New();
+  this->m_XmlReader = PSMCommandLineClass::ProjectReaderType::New();
+  this->m_XmlReader->SetFileName(this->m_ProjectParameterFile);
+  this->m_XmlReader->Update();
+  
+  this->m_Project = PSMCommandLineClass::ProjectType::New();
+  this->m_Project = m_XmlReader->GetOutput();
+  //itk::PSMProject::Pointer m_Project = m_XmlReader->GetOutput();
+  
+  const std::vector<std::string> &dt_files = this->m_Project->GetDistanceTransforms();
+  this->m_Filter = PSMCommandLineClass::EntropyModelFilterType::New();
+  std::cout << "Reading distance transforms ..." << std::endl;
+  for (unsigned int i = 0; i < dt_files.size(); i++)
+  {
+    itk::ImageFileReader<PSMCommandLineClass::ImageType>::Pointer reader =
+    itk::ImageFileReader<PSMCommandLineClass::ImageType>::New();
+    //reader->SetImageIO(MetaImageIO::New());
+    reader->SetFileName(input_path_prefix + dt_files[i]);
+    reader->Update();
+    
+    std::cout << "  " << dt_files[i] << std::endl;
+    this->m_Filter->SetInput(i, reader->GetOutput());
+  }
+  std::cout << "Done!" << std::endl;
+  // Load the model initialization.  It should be specified as a model with a name.
+  const std::vector<std::string> &pt_files = this->m_Project->GetModel(std::string("initialization"));
+  std::cout << "Reading the initial model correspondences ..." << std::endl;
+  for (unsigned int i = 0; i < pt_files.size(); i++)
+  {
+    // Read the points for this file and add as a list
+    std::vector<PSMCommandLineClass::EntropyModelFilterType::PointType> c;
+    
+    int counter = 0;
+    // Open the ascii file.
+    std::ifstream in( (input_path_prefix + pt_files[i]).c_str() );
+    if ( !in )
+    {
+      std::cerr << "Could not open point file for input." << std::endl;
+      break;
+    }
+    
+    // Read all of the points, one point per line.
+    while (in)
+    {
+      PSMCommandLineClass::EntropyModelFilterType::PointType pt;
+      
+      for (unsigned int d = 0; d < 3; d++)
+      {
+        in >> pt[d];
+      }
+      c.push_back(pt);
+      counter++;
+    }
+    // This algorithm pushes the last point twice
+    c.pop_back();
+    in.close();
+    
+    this->m_Filter->SetInputCorrespondencePoints(i,c);
+    
+    std::cout << "  " << pt_files[i] << std::endl;
+  }
+  std::cout << "Done!" << std::endl;
+  this->ReadInputParameters();
+}
+  
+template <unsigned int VDimension>
+void PSMCommandLineClass<VDimension>
+::ReadInputParameters()
+{
+  this->m_ProcrustesRegistration = PSMCommandLineClass::ProcrustesRegistrationType::New();
+  /*Added*/
+  //itk::PSMProjectReader::Pointer m_XmlReader = itk::PSMProjectReader::New();
+
+  //m_XmlReader->SetFileName(this->m_ProjectParameterFile);
+  //m_XmlReader->Update();
+  
+  //itk::PSMProject::Pointer m_Project = m_XmlReader->GetOutput(); /*End Added*/
+  
+  //  Provide some default parameters
+  double regularization_initial   = 100.0f;
+  double regularization_final     = 5.0f;
+  double regularization_decayspan = 2000.0f;
+  double tolerance                = 1.0e-8;
+  unsigned int maximum_iterations = 200000;
+  unsigned int procrustes_interval = 10;
+  
+  // Read the optimization parameters and set them in the filter
+  std::cout << "Optimization parameters: " << std::endl;
+  if ( this->m_Project->HasOptimizationAttribute("regularization_initial") )
+  {
+    regularization_initial = this->m_Project->GetOptimizationAttribute("regularization_initial");
+    this->m_Filter->SetRegularizationInitial(regularization_initial);
+    std::cout << "    regularization_initial = " << regularization_initial << std::endl;
+  }
+  if ( this->m_Project->HasOptimizationAttribute("regularization_final") )
+  {
+    regularization_final = this->m_Project->GetOptimizationAttribute("regularization_final");
+    this->m_Filter->SetRegularizationFinal(regularization_final);
+    std::cout << "      regularization_final = " << regularization_final << std::endl;
+  }
+  if ( this->m_Project->HasOptimizationAttribute("regularization_decayspan") )
+  {
+    regularization_decayspan = this->m_Project->GetOptimizationAttribute("regularization_decayspan");
+    this->m_Filter->SetRegularizationDecaySpan(regularization_decayspan);
+    std::cout << "  regularization_decayspan = " << regularization_decayspan << std::endl;
+  }
+  if ( this->m_Project->HasOptimizationAttribute("tolerance") )
+  {
+    tolerance = this->m_Project->GetOptimizationAttribute("tolerance");
+    this->m_Filter->SetTolerance(tolerance);
+    std::cout << "                 tolerance = " << tolerance << std::endl;
+  }
+  if ( this->m_Project->HasOptimizationAttribute("maximum_iterations") )
+  {
+    maximum_iterations = static_cast<unsigned int>(this->m_Project->GetOptimizationAttribute("maximum_iterations"));
+    this->m_Filter->SetMaximumNumberOfIterations(maximum_iterations);
+    std::cout << "        maximum_iterations = " << maximum_iterations << std::endl;
+  }
+  // Set Procrustes interval for ProcrustesRegistration
+  if ( this->m_Project->HasOptimizationAttribute("procrustes_interval") )
+  {
+    procrustes_interval = static_cast<unsigned int>(this->m_Project->GetOptimizationAttribute("procrustes_interval"));
+    this->m_ProcrustesRegistration->SetProcrustesInterval(procrustes_interval);
+    std::cout << "        procrustes_interval = " << procrustes_interval << std::endl;
+  }
+  // Set ParticleSystem for ProcrustesRegistration
+  this->m_ProcrustesRegistration->SetPSMParticleSystem(this->m_Filter->GetParticleSystem());
+}
+
+template <unsigned int VDimension>
+void PSMCommandLineClass<VDimension>
+::WriteOutputs(std::string output_path)
+{
+  const std::vector<std::string> &out_files = this->m_Project->GetModel(std::string("optimized"));
+  if (out_files.size() != this->m_Filter->GetParticleSystem()->GetNumberOfDomains())
+  {
+    std::cerr << "Number of output files does not match the number of particle domains (inputs)." << std::endl;
+  }  
+  else
+  {
+    for (unsigned int d = 0; d < this->m_Filter->GetParticleSystem()->GetNumberOfDomains(); d++)
+    {
+      // Open the output file.
+      std::string fname = output_path + out_files[d];
+      std::ofstream out( fname.c_str() );
+      if ( !out )
+      {
+        std::cerr << "Could not open point file for output: " << std::endl;
+        break;
+      }
+      else
+      {
+        for (unsigned int j = 0; j < this->m_Filter->GetParticleSystem()->GetNumberOfParticles(d); j++)
+        {
+          for (unsigned int i = 0; i < 3; i++)
+          {
+            out <<  this->m_Filter->GetParticleSystem()->GetPosition(j,d)[i]  << " ";
+          }
+          out << std::endl;
+        }
+      }
+    } // end for number of domains
+  }
+}
+  
+template <unsigned int VDimension>
+void PSMCommandLineClass<VDimension>
+::Run( const char *fname, std::string input_path_prefix, std::string output_path )
+{
+  // Set the name of the file containing project parameters
+  this->SetProjectParameterFileName(fname);
+  // Read the input point sets
+  this->ReadInputs(input_path_prefix);
+  // Create the callback function to run Procrustes
+  m_IterateCmd = itk::MemberCommand<PSMCommandLineClass>::New();
+  m_IterateCmd->SetCallbackFunction(this, &PSMCommandLineClass::IterateCallback);
+  // Add the observer to the filter which will run depending on the iteration count
+  this->m_Filter->AddObserver(itk::IterationEvent(), m_IterateCmd);
+  // Run the filter
+  this->m_Filter->Update();
+  // TODO: Is this error message really required?
+  if (this->m_Filter->GetNumberOfElapsedIterations() >= this->m_Filter->GetMaximumNumberOfIterations())
+  {
+    std::cerr << "Optimization did not converge based on tolerance criteria." << std::endl;
+  }
+  // Write out the optimized point sets
+  this->WriteOutputs(output_path);
+}
+
+template <unsigned int VDimension>
+void PSMCommandLineClass<VDimension>
+::SetProjectParameterFileName( const char *fname )
+{
+  this->m_ProjectParameterFile = fname;
+}
+// Explicit instantiation
+//template void PSMCommandLineClass<3>::New();
+template void PSMCommandLineClass<3>::Run( const char *fname, std::string input_path_prefix, std::string output_path );
+} // end namespace itk
+
+//#endif
