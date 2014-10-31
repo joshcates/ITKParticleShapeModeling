79441b0694155f6ad1472a228826c73ecfc92c27
sbengali <salma.bengali@carma.utah.edu>
2014-10-31 17:48:42 -0600

    Added Nrrd IO factory

--- src/itkIncludeRequiredIOFactories.h
+++ src/itkIncludeRequiredIOFactories.h
@@ -20,11 +20,13 @@
 #include "itkGDCMImageIOFactory.h"
 #include "itkMetaImageIOFactory.h"
 #include "itkObjectFactoryBase.h"
+#include "itkNrrdImageIOFactory.h"
 
 /** Fixed ITK runtime error where image format is not recognized. */
 void RegisterRequiredFactories(){
   itk::ObjectFactoryBase::RegisterFactory( itk::MetaImageIOFactory::New() );
   itk::ObjectFactoryBase::RegisterFactory( itk::GDCMImageIOFactory::New() );
+  itk::ObjectFactoryBase::RegisterFactory( itk::NrrdImageIOFactory::New() );
 }
 
 #endif
